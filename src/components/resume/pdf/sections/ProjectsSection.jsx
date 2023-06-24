159e6b5724a7382cdbd345abb655dee1d15e76cc
Dileep <33929926+dileep8417@users.noreply.github.com>
2023-06-24 08:15:05 +0530

    Update ProjectsSection.jsx

--- src/components/resume/pdf/sections/ProjectsSection.jsx
+++ src/components/resume/pdf/sections/ProjectsSection.jsx
@@ -5,27 +5,27 @@ const ProjectsSection = ({ styles, projects }) => {
     return (
         <View style={styles.section}>
             <Text style={styles.heading}>Recent Projects</Text>
-            
-            {projects.map(project => {
-                const title = project.fields.projectName.trim();
-                const description = project.fields.description.trim();
-                const sourceCode = project.fields.sourceCode.trim();
-                const demo = project.fields.demo.trim();
+            <View style={styles.projects}>
+                {projects.map(project => {
+                    const title = project.fields.projectName.trim();
+                    const description = project.fields.description.trim();
+                    const sourceCode = project.fields.sourceCode.trim();
+                    const demo = project.fields.demo.trim();
 
-                return (
-                    <View key={project.id} style={styles.project}>
-                        <View style={styles.flexContainer}>
-                            <Text style={{...styles.subHeading, ...{marginBottom: '0'}}}>{title !== '' && title}</Text>
-                            <View style={styles.projectLinks}>
-                                {sourceCode.length > 7 && <Link style={styles.projectLink} src={sourceCode}>Source code</Link>}
-                                {demo.length > 7 && <Link style={styles.projectLink} src={demo}>Demo</Link>}
+                    return (
+                        <View key={project.id} style={styles.project}>
+                            <View style={styles.flexContainer}>
+                                <Text style={{...styles.subHeading, ...{marginBottom: '0'}}}>{title !== '' && title}</Text>
+                                <View style={styles.projectLinks}>
+                                    {sourceCode.length > 7 && <Link style={styles.projectLink} src={sourceCode}>Source code</Link>}
+                                    {demo.length > 7 && <Link style={styles.projectLink} src={demo}>Demo</Link>}
+                                </View>
                             </View>
+                            <Text style={styles.projectDescription}>{description !== '' && description}</Text>
                         </View>
-                        <Text style={styles.projectDescription}>{description !== '' && description}</Text>
-                    </View>
-                )
-            })}
-
+                    )
+                })}
+            </View>
         </View>
     );
 }
